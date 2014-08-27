5f6ce5dfd49d19c9fcce27083fd63af53b28fc51
kawai-hiroyuki <kawai.hiroyuki7@gmail.com>
2014-08-28 00:19:20 +0900

    I tried CWDropDownMenu https://github.com/kevinrandrup/CWDropDownMenu

---
+++ LikeIBooksDropDownMenu/DropDownButton.h
@@ -0,0 +1,13 @@
+//
+//  UIButton+DropDown.h
+//  LikeIBooksDropDownMenu
+//
+//  Created by obumin on 2014/08/27.
+//  Copyright (c) 2014年 Kawai Hiroyuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (DropDown)
+
+@end
---
+++ LikeIBooksDropDownMenu/DropDownButton.m
@@ -0,0 +1,13 @@
+//
+//  UIButton+DropDown.m
+//  LikeIBooksDropDownMenu
+//
+//  Created by obumin on 2014/08/27.
+//  Copyright (c) 2014年 Kawai Hiroyuki. All rights reserved.
+//
+
+#import "UIButton+DropDown.h"
+
+@implementation UIButton (DropDown)
+
+@end
